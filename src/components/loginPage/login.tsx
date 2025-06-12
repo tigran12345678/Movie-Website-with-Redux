fc697fda3a10fbfe1171a193a3ee80f4d47b984a
Tigran <mr.tigran.aslanyan@gmail.com>
2025-06-12 12:49:01 +0400

    fixed the color of borders

--- src/components/loginPage/login.tsx
+++ src/components/loginPage/login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../config/firebase";
-import { signInWithEmailAndPassword} from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { HOME_PATH, MAINPAGE_PATH } from "../../paths/Paths";
 import { setCurrentUser } from "../../store/Authentication/authSlice";
 import { useDispatch } from "react-redux";
@@ -9,21 +9,21 @@ import { Button, Input } from "antd";
 import './Login.css'
 
 export default function Login() {
-  const [email, setEmail]       = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError]       = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
 
   const handleEmailSignIn = async () => {
     try {
-      const signInAttempt =  await signInWithEmailAndPassword(auth, email, password);
-      
-      sessionStorage.setItem("userToken",signInAttempt.user.uid );
+      const signInAttempt = await signInWithEmailAndPassword(auth, email, password);
+
+      sessionStorage.setItem("userToken", signInAttempt.user.uid);
       dispatch(setCurrentUser(sessionStorage.getItem("userToken")));
       navigate(MAINPAGE_PATH);
-      
+
     } catch (err: any) {
       setError(err.message);
     }
@@ -57,8 +57,8 @@ export default function Login() {
       <Button onClick={handleEmailSignIn}>Log In</Button>
       <br />
 
-  
-      <Button onClick = {() => navigate(HOME_PATH)}>Back Home</Button>
+
+      <Button onClick={() => navigate(HOME_PATH)}>Back Home</Button>
     </div>
   );
 }
